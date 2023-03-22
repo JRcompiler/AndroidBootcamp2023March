d0656106c504e01fed0ac39f3734400f5434b2ba
Mustafa Kılıç <120056395+JRcompiler@users.noreply.github.com>
2023-03-22 13:37:56 +0300

    Add files via upload
    Ödev 1

---
+++ DegiskenOdev.kt
@@ -0,0 +1,141 @@
+package com.info.odev
+
+fun main(){
+    var sehir:String = "Hatay"
+    println("Şehir : $sehir")
+
+    var ulke:String = "Türkiye"
+    println("Ülke : $ulke")
+
+    var telefon:String = "0530 000 00 00"
+    println("Telefon : $telefon")
+
+    var postaKodu:Int = 31200
+    println("Posta Kodu : $postaKodu")
+
+    var eMail:String = "mstfaklc9@gmail.com"
+    println("E-Mail : $eMail")
+
+    var meslek:String = "Mobil Uygulama Geliştirici"
+    println("Meslek : $meslek")
+
+    var stokMiktari:Int = 1000
+    println("Stok Miktarı : $stokMiktari")
+
+    var musteriAdi:String = "Mustafa Kılıç"
+    println("Müşteri Adı : $musteriAdi")
+
+    var bakiye:Int = 3000
+    println("Bakiye : $bakiye ₺")
+
+    var dogumGunu:String = "02.01.1994"
+    println("Doğum Günü : $dogumGunu")
+
+    var maas:Int = 15000
+    println("Maaş : $maas ₺")
+
+    var mediniDurum:String = "Bekar"
+    println("Medeni Durum : $mediniDurum")
+
+    var urunYorum:Float = 4.5.toFloat()
+    println("Ürün Yorum : $urunYorum")
+
+    var odemeTarih:String = "31.12.2023"
+    println("Ödeme Tarihi : $odemeTarih")
+
+    var odeme:String = "1000"
+    println("Ödeme : $odeme ₺")
+
+    var siparis:String = "500"
+    println("Sipariş : $siparis")
+
+    var arabaModel:String = "BMW"
+    println("Araba Model : $arabaModel")
+
+    var kitapAdı:String = "Anna Karenina"
+    println("Kitap Adı : $kitapAdı")
+
+    var yayinlamaTarih:String = "1873-1877"
+    println("Yayınlanma Tarihi : $yayinlamaTarih")
+
+    var ındırımMıktarı:String = "50"
+    println("İndirim Miltarı : $ındırımMıktarı ₺")
+
+    var odaSayisi:Int = 5
+    println("Oda Sayısı : $odaSayisi")
+
+    var enlem:Double = 5.2
+    println("Enlem : $enlem")
+
+    var boylam:Double = 9.3
+    println("Boylam : $boylam")
+
+    var urunAdi:String = "Bilgisayar"
+    println("Ürün Adı : $urunAdi")
+
+    var yemekFiyat:Int = 200
+    println("Yemek Fiyatı : $yemekFiyat ₺")
+
+    var marka:String = "BMW"
+    println("Marka : $marka")
+
+    var muzikAdi:String = "Wicked Game"
+    println("Müzik Adı : $muzikAdi")
+
+    var videoSuresi:Double = 4.05
+    println("Video Süresi : $videoSuresi dk")
+
+    var urunPuanı:Double = 4.5
+    println("Ürün Puanı : $urunPuanı")
+
+    var resimAdi:String = "Çicek"
+    println("Resim Adı : $resimAdi")
+
+    var dosyaFormatı:String = "PDF"
+    println("Dosya Formatı : $dosyaFormatı")
+
+    var renk:String = "Kırmızı"
+    println("Renk : $renk")
+
+    var renkKodu:String = "ae0000"
+    println("Renk Kodu : $renkKodu")
+
+    var telefonModeli:String = "2201116TG"
+    println("Telefon Modeli : $telefonModeli")
+
+    var ekranBoyutu:Double = 6.5
+    println("Ekran Boyutu : $ekranBoyutu inç")
+
+    var agirlik:Int = 220
+    println("Ağırlık : $agirlik gr")
+
+    var ulusalGun:String = "30 Ağustos"
+    println("ulusal Gün : $ulusalGun")
+
+    var tatilgunu:String = "Cumartesi"
+    println("Tatil Günü : $tatilgunu")
+
+    var rezervasyonTarihi:String = "01.06.2023"
+    println("Rezervasyon Tarihi : $rezervasyonTarihi")
+
+    var sokakAdi:Int = 740
+    println("Sokak Adı : $sokakAdi")
+
+    var otobüsHattı:String = "Sakarya Mah. - Sahil"
+    println("Otobüs Hattı : $otobüsHattı")
+
+    var kalandakika:Int = 44
+    println("Kalan Dakika : $kalandakika")
+
+    var takipKodu:String = "0CAEB62H29T"
+    println("Takip Kodu : $takipKodu")
+
+    var kuponSuresi:Int = 24
+    println("Kupon Süresi : $kuponSuresi gün")
+
+    var kuponKodu:String = "62H29"
+    println("Kupon Kosu : $kuponKodu")
+
+    var faturaAdres:String = "Hatay/İskenderun"
+    println("Fatura Adresi : $faturaAdres")
+}
